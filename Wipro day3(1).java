48aed81a7c18b82a6d246556af893b529d968b96
Janani715 <65948404+Janani715@users.noreply.github.com>
2020-05-28 19:15:24 +0300

    Add files via upload

---
+++ Wipro day3(1).java
@@ -0,0 +1,28 @@
+1)
+public class Main
+{
+	public static void main(String args[]) {
+		System.out.println(args[0] +" Technologies " +args[1]);
+	}
+}
+2)
+public class Main
+{
+	public static void main(String args[]) {
+		System.out.println(" Welcome " +args[0]);
+	}
+}
+3)
+
+public class Main
+{
+	public static void main(String[] args) {
+	    int a=Integer.parseInt(args[0]);
+	    int b=Integer.parseInt(args[1]);
+	    int c=a+b;
+		System.out.println("The Sum of " +args[0]+ " and " +args[1]+ " is "+c);
+	}
+}
+
+
+
